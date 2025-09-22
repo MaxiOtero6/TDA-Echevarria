d2b6f3ae0da5f074e42817996d5b07801cd625d1
ipuj <ipujato@fi.uba.ar>
2025-09-21 23:33:32 -0300

    feat: added greedy algorithm + plus tests

--- TP1/Greedy/algo.py
+++ TP1/Greedy/algo.py
@@ -1,2 +1,17 @@
 def greedy(arr: list[int]):
-    pass
+    intervalos = []
+    subintervalo = []
+    suma = 0
+
+    for i, numero in enumerate(arr):
+        suma += numero
+        if suma > 0 or (i < len(arr) - 1 and (suma + arr[i+1]) > 0):
+            subintervalo.append(numero)
+        else:
+            suma = 0
+            if subintervalo:
+                intervalos.append(subintervalo)
+            subintervalo = []
+    if subintervalo:
+                intervalos.append(subintervalo)
+    return len(intervalos)
--- TP1/Greedy/greedy_test.py
+++ TP1/Greedy/greedy_test.py
@@ -2,6 +2,13 @@ import pytest
 from .algo import greedy
 
 test_cases = [([3, -5, 7, -4, 1, -8, 3, -7, 5, -9, 5, -2, 4], 3)]
+test_cases += [([1, -1, 1, -1, 1, 1], 1)]
+test_cases += [([-1, -2, -3, -4], 0)]
+test_cases += [([5, 6, -2, -1, 4, -3, 2], 1)]
+test_cases += [([0, 0, 0, 0], 0)]
+test_cases += [([10, -5, 3, -2, 1, -1, 4, -6, 2], 1)]
+test_cases += [([1, 2, 3, -6, 4, 5, -10, 6], 1)]
+test_cases += [([-2, -3, 4, -1, -2, 1, 5, -3], 2)]
 
 
 @pytest.mark.parametrize("arr, expected", test_cases)
