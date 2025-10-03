1564a2cbb8b929afb5c8d1812307fa19c699f4c4
ipuj <ipujato@fi.uba.ar>
2025-10-03 19:58:19 -0300

    fix greedy identation

--- TP1/Greedy/algo.py
+++ TP1/Greedy/algo.py
@@ -1,12 +1,12 @@
 def turns_positive(arr: list[int], i: int, suma: int) -> bool:
     for j in range(i + 1, len(arr)):
-            suma += arr[j]
-            if suma > 0:
-                return True
+        suma += arr[j]
+        if suma > 0:
+            return True
     return False
 
 def greedy(arr: list[int]):
-    intervalos = []
+    intervalos = 0
     subintervalo = []
     suma = 0
 
@@ -17,11 +17,12 @@ def greedy(arr: list[int]):
         elif (numero > 0 and not subintervalo):
             subintervalo.append(numero)
             suma = numero
+
         if not turns_positive(arr, i, suma):
             suma = 0
             if subintervalo and len(subintervalo) > 1:
-                intervalos.append(subintervalo)
+                intervalos += 1
             subintervalo = []
     if subintervalo and len(subintervalo) > 1:
-                intervalos.append(subintervalo)
-    return len(intervalos)
+        intervalos += 1
+    return intervalos
