4448494f3b8ffa8471177ff6c6404f514f79f621
Daniel Mamani <danymamani1002@gmail.com>
2025-10-05 16:52:54 -0300

    test: add test cases to dinamica_test

--- TP1/Dinamica/dinamica_test.py
+++ TP1/Dinamica/dinamica_test.py
@@ -1,7 +1,13 @@
 import pytest
 from .dinamica import dinamica
 
-test_cases = []
+test_cases = [
+    ([-2, 1, -3, 4, -1, 2, 1, -5, 4], (6, 3, 6)),
+    ([1, 2, 3, 4, 5], (15, 0, 4)),
+    ([-1, -2, -3, -4], (-1, 0, 0)),
+    ([0, 0, 0, 0], (0, 0, 0)),
+    ([3, -2, 5, -1], (6, 0, 2)),
+]
 
 
 @pytest.mark.parametrize("arr, expected", test_cases)
